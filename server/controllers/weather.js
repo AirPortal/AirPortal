7bbc86c834f59cf72b5de61f7e679ccfe2f17887
Harrison Ng <tplawyer@gmail.com>
2016-09-24 15:26:22 -0700

    Bag.js

--- server/controllers/weather.js
+++ server/controllers/weather.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const apiKey = '89e15931434731aefdaa04920ec60e44';
 
 module.exports = {
   getWeather: (clientReq, clientRes) => {
@@ -7,7 +8,7 @@ module.exports = {
       port: 443,
       path: '/weather/v1/current/HAM?temperatureScale=C',
       method: 'GET',
-      headers: { 'x-apiKey': '89e15931434731aefdaa04920ec60e44' }
+      headers: { 'x-apiKey': apiKey }
     };
 
     var apiReq = https.request(options, apiRes => {
@@ -15,7 +16,6 @@ module.exports = {
       console.log('headers:', apiRes.headers);
 
       apiRes.on('data', d => {
-        // process.stdout.write(d);
         clientRes.send(d);
       });
     });
--- server/routes.js
+++ server/routes.js
@@ -8,11 +8,13 @@ const upload = multer({
 const userController = require(__dirname + '/controllers/user');
 const uploadController = require(__dirname + '/controllers/upload');
 const weatherController = require(__dirname + '/controllers/weather');
+const bagController = require(__dirname + '/controllers/bag');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/upload', upload.single('profileImage'), uploadController.upload);
 
 router.get('/weather', weatherController.getWeather);
+router.get('/getBags', bagController.getBagsForPassenger);
 
 module.exports = router;
