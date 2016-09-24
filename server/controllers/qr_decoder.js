704e50823dfa90b95765339638e6efeef6ca3430
Harrison Ng <tplawyer@gmail.com>
2016-09-24 15:56:51 -0700

    ticket

--- server/controllers/qr_decoder.js
+++ server/controllers/qr_decoder.js
@@ -1,2 +1,19 @@
-// https://www.npmjs.com/package/qrcode-reader
-const QrCode = require('qrcode-reader');
+const Canvas = require('canvas');
+const Image = Canvas.Image;
+const qrcode = require('jsqrcode')(Canvas);
+
+// const filename = __dirname + '/qrcode.png';
+
+module.exports = filename => {
+  const image = new Image();
+  image.onload = () => {
+    var result;
+    try {
+      result = qrcode.decode(image);
+      console.log('result of qr code: ' + result);
+    } catch(e) {
+      console.log('unable to read qr code');
+    }
+  };
+  image.src = filename;
+};
--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -1,16 +1,20 @@
+const qrDecoder = require('./qr_decoder');
+
 module.exports = {
   upload: (req, res, next) => {
     if(req.file) {
       console.log('Uploading File');
-      var profileImageOriginlName=req.file.originalname;
-      var profileImageName=req.file.name;
-      var profileImageMime=req.file.mimetype;
-      var profileImagePath=req.file.path;
-      var profileImageExt=req.file.extension;
-      var profileImageSize=req.file.size;
+      // var profileImageOriginlName=req.file.originalname;
+      // var profileImageName=req.file.name;
+      // var profileImageMime=req.file.mimetype;
+      var profileImagePath = req.file.path;
+      // var profileImageExt=req.file.extension;
+      // var profileImageSize=req.file.size;
       console.log(profileImagePath);
       const response = JSON.stringify({msg: 'file uploaded'});
       res.send(response);
+
+      qrDecoder('../' + profileImagePath);
     }
   }
 };
