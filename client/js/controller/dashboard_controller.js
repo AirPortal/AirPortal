5ac2956bb948113425a4d499915a4d30b701094e
Harrison Ng <tplawyer@gmail.com>
2016-09-25 09:37:10 -0700

    update

--- client/js/controller/dashboard_controller.js
+++ client/js/controller/dashboard_controller.js
@@ -1,11 +1,11 @@
 app.controller('dashboardController', ['$scope', 'dashboardService', ($scope, dashboardService) => {
+  $scope.luggage = 'Ready!';
+  $scope.waittime = 30;
+  $scope.taxi = 24;
+  $scope.subway = 56;
   dashboardService.weather().then(res => {
     $scope.temperatureScale = res.data.temperatureScale;
     $scope.temp = res.data.currentWeather.temperature;
-    $scope.luggage = 'Ready!';
-    $scope.waittime = 30;
-    $scope.taxi = 24;
-    $scope.subway = 56;
   }, () => {
 
   });
