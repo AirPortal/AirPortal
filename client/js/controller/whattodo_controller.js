7d168d7e494a31fded74df7eb054c40f144fb170
Harrison Ng <tplawyer@gmail.com>
2016-09-25 10:53:59 -0700

    what to do

--- client/js/controller/whattodo_controller.js
+++ client/js/controller/whattodo_controller.js
@@ -1,4 +1,4 @@
-app.controller('whattodoController', ['$scope', 'whattodoService', ($scope, whattodoService) => {
+app.controller('whattodoController', ['$scope', 'whattodoService', '$timeout', ($scope, whattodoService, $timeout) => {
   $scope.flightNo = "AF747";
   $scope.destination = "SFO";
   $scope.flightstatus = "on time";
@@ -6,11 +6,30 @@ app.controller('whattodoController', ['$scope', 'whattodoService', ($scope, what
   $scope.minutesLeft = 40;
   $scope.act = null;
   $scope.venue = null;
-  whattodoService.whattodo().then((res) => {
-    console.log(res);
-    $scope.act = res.action;
-    $scope.venue = res.venue;
+  const getWhatToDo = () => {
+    return new Promise((resolve, reject) => {
+      whattodoService.whattodo().then((res) => {
+        console.log(res);
+        $scope.act = res.action;
+        $scope.venue = res.venue;
+
+        $timeout(() => {
+          $('.modal-trigger').leanModal({
+            dismissible: false,
+            in_duration: 200,
+            out_duration: 200,
+            complete: () => {
+              $timeout(getWhatToDo, 1000);
+              console.log("closed");
+            }
+          });
+          resolve();
+        }, 2);
+      });
+    });
+  };
+  $scope.getWhatToDo = getWhatToDo;
+  getWhatToDo().then(() => {
+    $('#modal1').openModal();
   });
-  $('.modal-trigger').leanModal();
-  $('#modal1').openModal();
 }]);
