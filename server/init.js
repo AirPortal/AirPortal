7ea25d7ade3ca1b7a43fa678abcd0dadd9c27bd2
Harrison Ng <tplawyer@gmail.com>
2016-09-24 23:02:57 -0700

    Added default ticket

--- server/init.js
+++ server/init.js
@@ -1,5 +1,6 @@
 const mg = require('mongoose');
 const todoModel = require(__dirname + '/models/todo');
+const ticketModel = require(__dirname + '/models/ticket');
 
 todoModel.find({}, (err, todos) => {
   if(err) console.log('error connecting to todo collection.');
@@ -17,6 +18,26 @@ todoModel.find({}, (err, todos) => {
     todoModel.create({
       action: 'Go visit',
       venue: 'Orchid Garden'
+    });
+  }
+});
+
+ticketModel.find({}, (err, todos) => {
+  if(err) console.log('error connecting to ticket collection.');
+  if(todos.length === 0) {
+    ticketModel.create({
+      "ticket_number" : 1,
+      "flight_class" : "economy",
+      "row" : "15",
+      "boarding_time" : new Date("2016-09-24T17:55:10.349+0000"),
+      "flight_number" : "3K202",
+      "gate" : null,
+      "destination" : "SIN",
+      "origin" : "CGK",
+      "terminal" : 1,
+      "bag_tags" : [
+        "Kamis"
+      ]
     });
   }
 });
