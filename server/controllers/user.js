3ecbab23935578cf4cb7cd04768d9fe1075cfe7b
Harrison Ng <tplawyer@gmail.com>
2016-09-24 20:25:33 -0700

    find my ticketNumber

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,7 +4,7 @@ const weatherController = require(__dirname + '/weather');
 module.exports = {
   login: (req, res) => {
     const ticketNumber = req.body.ticketNumber;
-
+    console.log(req.body);
     ticketModel.findOne({ ticket_number: ticketNumber}, (err, ticket) => {
       const responseObj = {msg: err};
       if(err) res.send(responseObj);
