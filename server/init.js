b89f6b70f517f03995a6071176dc1e1d1cff045b
Harrison Ng <tplawyer@gmail.com>
2016-09-24 21:00:55 -0700

    comment out init

--- server/init.js
+++ server/init.js
@@ -1,21 +1,21 @@
 const mg = require('mongoose');
 const todoModel = require(__dirname + '/models/todo');
-
-mg.connection.db.listCollections({name: 'Todo'}).next((err, todo) => {
-  if(!todo) {
-    todoModel.create({
-      action: 'Go grab a bite at',
-      venue: 'Burger King'
-    });
-
-    todoModel.create({
-      action: 'Get a mani pedi at',
-      venue: 'Airport Wellness Oasis'
-    });
-
-    todoModel.create({
-      action: 'Gp visit',
-      venue: 'Orchid Garden'
-    });
-  }
-});
+//
+// mg.connection.db.listCollections({name: 'Todo'}).next((err, todo) => {
+//   if(!todo) {
+//     todoModel.create({
+//       action: 'Go grab a bite at',
+//       venue: 'Burger King'
+//     });
+//
+//     todoModel.create({
+//       action: 'Get a mani pedi at',
+//       venue: 'Airport Wellness Oasis'
+//     });
+//
+//     todoModel.create({
+//       action: 'Gp visit',
+//       venue: 'Orchid Garden'
+//     });
+//   }
+// });
