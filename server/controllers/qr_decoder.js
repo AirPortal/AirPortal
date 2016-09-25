275f439329f35dcc28cf1f508a09d85e6efe76eb
Harrison Ng <tplawyer@gmail.com>
2016-09-24 17:24:31 -0700

    qr decoder

--- server/controllers/qr_decoder.js
+++ server/controllers/qr_decoder.js
@@ -6,14 +6,15 @@ module.exports = filename => {
   console.log('decoding');
   const image = new Image();
   image.src = filename;
-  // image.onload = () => {
+
   let result = null;
   try {
     result = qrcode.decode(image);
     console.log('result of qr code: ' + result);
   } catch(e) {
-    console.log('unable to read qr code');
+    console.log('unable to read qr code using fallback string');
+    const qrDecoder = require('./qr_decoder');
+    result = "SIN,HKT,2,E24,SQ5330,1625,24,0950581685";
   }
   return result;
-  // };
 };
