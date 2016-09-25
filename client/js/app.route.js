33fc9a041eab4a18840cdfaea94e83147e8f5346
Harrison Ng <tplawyer@gmail.com>
2016-09-24 22:55:23 -0700

    move index to templae

---
+++ client/js/app.route.js
@@ -0,0 +1,9 @@
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl : "./templates/landing.html"
+  })
+  .when("/", {
+    templateUrl : "./templates/landing.html"
+  })
+});
