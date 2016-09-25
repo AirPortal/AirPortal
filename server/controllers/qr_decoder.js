2a7b15440c2dbe3bfc66379510f0951c69269fb7
Harrison Ng <tplawyer@gmail.com>
2016-09-24 21:21:44 -0700

    fix concat'

--- server/controllers/qr_decoder.js
+++ server/controllers/qr_decoder.js
@@ -10,7 +10,7 @@ module.exports = filename => {
   let result = null;
   try {
     result = qrcode.decode(image);
-    console.log('result of qr code: ' + result);
+    console.log('result of qr code: '.concat(result));
   } catch(e) {
     console.log('unable to read qr code using fallback string');
     result = 'SIN,HKT,2,E24,SQ5330,1625,24,C,Lim,Zheng,0950581685';
