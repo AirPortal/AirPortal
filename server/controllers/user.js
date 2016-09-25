69a4ef6e05446c5f60686d21f6dfb8395fa59dbd
Harrison Ng <tplawyer@gmail.com>
2016-09-24 20:11:42 -0700

    Able to return model :

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,25 +1,14 @@
-const userModel = require(__dirname + '/../models/user');
 const ticketModel = require(__dirname + '/../models/ticket');
-
 const weatherController = require(__dirname + '/weather');
 
 module.exports = {
   login: (req, res) => {
-    // const user = new userModel();
-    // user.name = req.body.name;
-    // user.ticketNumber = req.body.ticketNumber;
-    // user.save((err, u) => {
-    //   if(err) return console.error(err);
-    //   const msg = {msg: 'user created'};
-    //   res.send(JSON.stringify(msg));
-    // });
     const ticketNumber = req.body.ticketNumber;
 
-    // userModel.populate('Ticket').findOne({ title: 'Once upon a timex.' })
-
-    const responseObject = {
-      name: ''
-    };
-    res.send(JSON.stringify(responseObject));
+    ticketModel.findOne({ ticket_number: ticketNumber}, (err, ticket) => {
+      const responseObj = {msg: err};
+      if(err) res.send(responseObj);
+      res.send(ticket);
+    });
   }
 };
