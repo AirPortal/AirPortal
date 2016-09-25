a1a8d08fe8ae3eac5cd73d0067124f4b67cd740a
Harrison Ng <tplawyer@gmail.com>
2016-09-24 22:58:03 -0700

    remove hash:

--- client/js/app.route.js
+++ client/js/app.route.js
@@ -1,9 +1,14 @@
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
   $routeProvider
   .when("/", {
     templateUrl : "./templates/landing.html"
   })
-  .when("/", {
-    templateUrl : "./templates/landing.html"
-  })
+  .when("/map", {
+    templateUrl : "./templates/map.html"
+  });
+
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
 });
