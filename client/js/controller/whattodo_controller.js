08954b2015a80126cbec333c3674fbb83dbc0fa7
Harrison Ng <tplawyer@gmail.com>
2016-09-25 09:46:06 -0700

    Add whattodo

---
+++ client/js/controller/whattodo_controller.js
@@ -0,0 +1,7 @@
+app.controller('whattodoController', ['$scope', ($scope) => {
+  $scope.flightNo = "AF747";
+  $scope.destination = "SFO";
+  $scope.flightstatus = "on time";
+  $scope.hoursLeft = 3;
+  $scope.minutesLeft = 40;
+}]);
